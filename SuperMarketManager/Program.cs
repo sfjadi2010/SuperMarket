bd416d5cde6f66640eeae19a5cc1000d25f95706
Salman Farsi <sfjadi2010@live.com>
2025-05-24 09:45:35 -0400

    Added Bootstrap and updated layout.

--- SuperMarketManager/Program.cs
+++ SuperMarketManager/Program.cs
@@ -3,6 +3,10 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
+app.UseRouting();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}"
