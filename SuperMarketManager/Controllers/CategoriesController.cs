c20873096f6b2cc1f18ed8a7dfb913bc3b128d7c
Salman Farsi <sfjadi2010@live.com>
2025-05-24 12:25:04 -0400

    Added Create form and related changes.

--- SuperMarketManager/Controllers/CategoriesController.cs
+++ SuperMarketManager/Controllers/CategoriesController.cs
@@ -7,14 +7,49 @@ namespace SuperMarketManager.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var categories = CategoriesRepository.GetCategories();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            var category = new Category();
+            return View(category);
         }
 
-        public IActionResult Edit(int? id)
+        [HttpPost]
+        public IActionResult Create(Category category)
         {
-            var category = new Category { Id = id.HasValue ? id.Value : 0 };
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.AddCategory(category);
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Edit([FromRoute] int? id)
+        {
+            var category = CategoriesRepository.GetCategoryById(id ?? 0);
 
             return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.UpdateCategory(category);
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            CategoriesRepository.DeleteCategory(id ?? 0);
+            return RedirectToAction("Index");
         }
     }
 }
--- SuperMarketManager/Models/Category.cs
+++ SuperMarketManager/Models/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace SuperMarketManager.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperMarketManager.Models;
 
 public class Category
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; } = default!;
-    public string Description { get; set; } = default!;
+    public string? Description { get; set; } = default!;
 }
